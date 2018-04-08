93f25a8ade610a09fa63c4618b853d5140879cc7
Thomas <thomasterebesi@googlemail.com>
2018-04-08 20:02:54 +0200

    Two-dimensional arrays with loops (244-255).

---
+++ Chapter 05/Examples/nested.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+const int CITIES = 5;
+const int YEARS = 4;
+
+int main()
+{
+	const char* cities[CITIES] = {
+		"Gribble City",
+		"Gribbletown",
+		"New Gribble",
+		"San Gribble",
+		"Gribble Vista"
+	};
+
+	int maxtemps[YEARS][CITIES] = {
+		{ 96, 100, 87, 101, 105 },
+		{ 96, 100, 91, 107, 104 },
+		{ 97, 101, 93, 108, 107 },
+		{ 98, 103, 95, 109, 108 }
+	};
+
+	std::cout << "Maximum temperatures for 2008 - 2011\n\n";
+	for (int city = 0; city < CITIES; ++city)
+	{
+		std::cout << cities[city] << ":\t";
+		for (int year = 0; year < YEARS; ++year)
+			std::cout << maxtemps[year][city] << "\t";
+		std::cout << std::endl;
+	}
+
+	int j = 5;
+	while (++j < 9)
+	{
+		std::cout << j++ << std::endl;
+	}
+
+	std::cin.get();
+	return 0;
+}
