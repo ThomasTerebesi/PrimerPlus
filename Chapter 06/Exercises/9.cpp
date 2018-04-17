7a6c4c9ff12a3eca067a7d9756e225746ede5512
Thomas <thomasterebesi@googlemail.com>
2018-04-17 23:39:54 +0200

    Finished exercises (301 - 303).

--- Chapter 06/Exercises/9.cpp
+++ Chapter 06/Exercises/9.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 
 const double THRESHOLD = 10000.0;
@@ -15,13 +16,30 @@ struct Contribution
 int main()
 {
 	std::cout << "Contribution tracker for the Society for the Preservation of Rightful Influence" << std::endl;
-	std::cout << "Enter the number of contributors you want to add: ";
+	
+	std::string filename;
+	std::ifstream inFile;
+
+
+	std::cout << "Enter a name of a file containing contribution data: ";
+	std::cin >> filename;
+
+
+	inFile.open(filename);
+	if (!inFile.is_open())
+	{
+		std::cout << "Could not open the file " << filename << std::endl;
+		std::cout << "Program terminating." << std::endl;
+
+		std::cin.get();
+
+		exit(EXIT_FAILURE);
+	}
 
 
 	unsigned short contributorNumber;
-	std::cin >> contributorNumber;
-
-	std::cin.get();
+	inFile >> contributorNumber;
+	inFile.get();
 
 
 	Contribution* contributions = new Contribution[contributorNumber];
@@ -29,14 +47,23 @@ int main()
 
 	for (int i = 0; i < contributorNumber; i++)
 	{
-		std::cout << "\nPlease enter the name of contributor #" << (i + 1) << ": ";
-		getline(std::cin, contributions[i].name);
+		getline(inFile, contributions[i].name);
+		inFile >> contributions[i].contributionAmount;
 
-		std::cout << "Please enter the amount " << contributions[i].name << " contributed: $";
-		std::cin >> contributions[i].contributionAmount;
-
-		std::cin.get();
+		inFile.get();
 	}
+
+
+	if (inFile.eof())
+	{
+		std::cout << "\nEnd of file reached." << std::endl;
+	}
+	else if (inFile.fail())
+		std::cout << "Input terminated by data mismatch." << std::endl;
+	else
+		std::cout << "Input terminated for unknown reason." << std::endl;
+
+	inFile.close();
 
 
 	std::cout.precision(2);
@@ -79,6 +106,7 @@ int main()
 		std::cout << "None." << std::endl;
 
 
+	std::cin.get();
 	std::cin.get();
 
 	delete[] contributions;
