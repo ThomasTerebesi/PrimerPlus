db0ea0f9d68ee2fa3f3fa3af6e9a6a08fdb77b95
Thomas <thomasterebesi@googlemail.com>
2018-05-14 23:52:01 +0200

    A new and improved string class (637 - 658).

--- Chapter 12/Examples/vegnews_good.cpp
+++ Chapter 12/Examples/vegnews_good.cpp
@@ -1,19 +1,20 @@
-// compile with strngbad.h and strngbad.cpp
+// compile with string1.h and string1.cpp
+// this uses the working String class
 
 #include <iostream>
-#include "strngbad.h"
+#include "string1.h"
 
-void CallMe1(StringBad &);
-void CallMe2(StringBad);
+void CallMe1(String &);
+void CallMe2(String);
 
 int main()
 {
 	{
 		std::cout << "Starting an inner block." << std::endl;
 
-		StringBad headline1("Celery Stalks at Midnight");
-		StringBad headline2("Lettuce Prey");
-		StringBad sports("Spinach Leaves Bowl for Dollars");
+		String headline1("Celery Stalks at Midnight");
+		String headline2("Lettuce Prey");
+		String sports("Spinach Leaves Bowl for Dollars");
 
 		std::cout << "\n";
 
@@ -27,20 +28,20 @@ int main()
 		std::cout << "headline2: " << headline2 << std::endl;
 
 		std::cout << "\nInitialize one object to another:" << std::endl;
-		StringBad sailor = sports;
+		String sailor = sports;
 		std::cout << "sailor: " << sailor << std::endl;
 
 		std::cout << "\n";
 
 		std::cout << "Assign one object to another:" << std::endl;
-		StringBad knot;
+		String knot;
 		knot = headline1;
 		std::cout << "knot: " << knot << std::endl;
 
 		std::cout << "\n";
 
 		std::cout << "Assign a string to an object: " << std::endl;
-		StringBad zap = "This works?";
+		String zap = "This works?";
 		std::cout << "zap: " << zap << std::endl;
 
 		std::cout << "\n";
@@ -55,13 +56,13 @@ int main()
 	return 0;
 }
 
-void CallMe1(StringBad & rsb)
+void CallMe1(String & rsb)
 {
 	std::cout << "\nString passed by reference: " << std::endl;
 	std::cout << "\t\"" << rsb << "\"" << std::endl;
 }
 
-void CallMe2(StringBad sb)
+void CallMe2(String sb)
 {
 	std::cout << "\nString passed by value: " << std::endl;
 	std::cout << "\t\"" << sb << "\"" << std::endl;
